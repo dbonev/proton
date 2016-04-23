722241642ce211126082678a78434ab1a56497d8
Dimitar Bonev <dimitar.bonev@gmail.com>
2016-04-23 15:41:20 +0300

    Fixing docker configuration

--- mongo_up.bash
+++ mongo_up.bash
@@ -1,2 +1,2 @@
 sudo docker build --tag dbonev/mongo ./mongo
-sudo docker run -d --name mongo -p 27017:27017 --net proton dbonev/mongo
+sudo docker run -d --name mongo -p 27017:27017 --net proton_proton dbonev/mongo
